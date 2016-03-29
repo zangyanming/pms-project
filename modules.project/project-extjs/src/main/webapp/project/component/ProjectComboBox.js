5b896b52c082a281f85ca07607c574249515d1b3
zangyanming <zyanming2002@163.com>
2016-03-29 16:50:14 +0800

    加入项目的下拉查询框

---
+++ modules.project/project-extjs/src/main/webapp/project/component/ProjectComboBox.js
@@ -0,0 +1,19 @@
+/**
+ * 合同选择下拉comboBox
+ * @author chenyanxu
+ */
+Ext.define('kalix.pms.project.component.ProjectComboBox', {
+    extend: 'kalix.view.components.common.BaseComboBox',
+    requires: [
+        'kalix.pms.project.store.ProjectStore'
+    ],
+    alias: 'widget.projectComboBox',
+    xtype: 'projectComboBox',
+    valueField: 'id',
+    displayField: 'projectName',
+    queryParam: 'projectName',
+    modelField:'projectId',
+    store: {
+        type: 'projectStore'
+    }
+});
